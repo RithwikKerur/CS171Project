a0bccc210b72590e01a3bd2e88a6b5acb6d810c1
Rithwik Kerur <rithwik.kerur@gmail.com>
2022-11-02 17:50:51 -0700

    added depth 1 AI

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -23,5 +23,46 @@ class StudentAI():
         index = randint(0,len(moves)-1)
         inner_index =  randint(0,len(moves[index])-1)
         move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
-        return move
+        print(move)
+        print(self.color)
+        print()
+        #self.board.make_move(move,self.color)
+        return self.find_best_move(moves)
+
+
+    def find_best_move(self, moves):
+
+        heuristic = None
+        best_move = None
+        print("AI")
+        for row in moves:
+            for move in row:
+                print(move)
+                self.board.make_move(move, self.color)
+
+                
+                if self.color == 2:
+                    temp = self.board.white_count - self.board.black_count
+                    if heuristic == None:
+                        heuristic = temp
+                        best_move= move
+                    elif temp > heuristic:
+                        heuristic = temp
+                        best_move = move
+                else:
+                    temp = self.board.black_count - self.board.white_count
+                    if heuristic == None:
+                        heuristic = temp
+                        best_move= move
+                    elif temp > heuristic:
+                        heuristic = temp
+                        best_move = move
+                self.board.undo()
+
+        print("best move")
+        print(best_move)
+        self.board.make_move(best_move, self.color) 
+        return best_move
+
+
+
