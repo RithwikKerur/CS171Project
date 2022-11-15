7d04284cc3ca2ccf8ef533f5ddc97a1d10fccc53
RithwikKerur <72475174+RithwikKerur@users.noreply.github.com>
2022-11-14 18:41:44 -0800

    moved Tools

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -30,39 +30,69 @@ class StudentAI():
         return self.find_best_move(moves)
 
 
+    def move_kings(self, move, color):
+        if color == 2:
+            if move[-1][0] == 0:
+                return True
+            return False
+        if color == 1:
+            if move[-1][0] == self.board.row - 1:
+                return True
+            return False
+
+    def get_heuristic(self, color): # returns the piece differential for the selected color
+        if color == 2: #we are counting the white differential
+            return self.board.white_count - self.board.black_count
+        return self.board.black_count - self.board.white_count
+
+
+
+    def get_opponent_best_move(self, move): # returns the best move the opponent can make
+        opp_color = 1
+        if self.color == 1:
+            opp_color = 2 #this ensures we always know the opponents color for testing
+        self.board.make_move(move, self.color)
+
+        moves = self.board.get_all_possible_moves(opp_color)
+
+        heuristic = None
+        best_move = None
+        for row in moves:
+            for move in row:
+                self.board.make_move(move, opp_color) 
+
+                temp = self.get_heuristic(opp_color)
+                if heuristic == None or temp > heuristic:
+                    heuristic = temp
+                    best_move = move
+                self.board.undo()
+
+
+        self.board.undo()
+        return heuristic
+
+
+
     def find_best_move(self, moves):
 
         heuristic = None
         best_move = None
-        print("AI")
-        for row in moves:
-            for move in row:
-                print(move)
-                self.board.make_move(move, self.color)
+        for piece in moves:
+            for move in piece:
+                if self.move_kings(move, self.color):
+                    row, col = move[0]
+                    if not self.board.board[row][col].is_king:
+                        best_move = move
+                        self.board.make_move(best_move, self.color) 
+                        return best_move
 
                 
-                if self.color == 2:
-                    temp = self.board.white_count - self.board.black_count
-                    if heuristic == None:
-                        heuristic = temp
-                        best_move= move
-                    elif temp > heuristic:
-                        heuristic = temp
-                        best_move = move
-                else:
-                    temp = self.board.black_count - self.board.white_count
-                    if heuristic == None:
-                        heuristic = temp
-                        best_move= move
-                    elif temp > heuristic:
-                        heuristic = temp
-                        best_move = move
-                self.board.undo()
+                opp_hueristic = self.get_opponent_best_move(move)
+                if heuristic == None or opp_hueristic < heuristic:
+                    heuristic = opp_hueristic
+                    best_move = move
+                
 
-        print("best move")
-        print(best_move)
+        
         self.board.make_move(best_move, self.color) 
         return best_move
-
-
-
