942f86905fadb1cc3c181cbfb1ae643bc97ad27a
RithwikKerur <72475174+RithwikKerur@users.noreply.github.com>
2022-11-15 01:05:59 -0800

    added recursive ai

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,6 +1,7 @@
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
+import sys
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 class StudentAI():
@@ -15,19 +16,17 @@ class StudentAI():
         self.opponent = {1:2,2:1}
         self.color = 2
     def get_move(self,move):
+        print('best move ')
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
+
+        print(f'color = {self.color}')
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        print(move)
-        print(self.color)
-        print()
-        #self.board.make_move(move,self.color)
-        return self.find_best_move(moves)
+        best_move, heuristic = self.recur_best_move(moves, 3, self.color)
+        self.board.make_move(best_move, self.color)
+        return best_move
 
 
     def move_kings(self, move, color):
@@ -41,9 +40,16 @@ class StudentAI():
             return False
 
     def get_heuristic(self, color): # returns the piece differential for the selected color
+        heuristic = 0
+        for row in range(self.board.row):
+                for col in range(self.board.col):
+                    if self.board.board[row][col].is_king and self.board.board[row][col].color == color:
+                        heuristic += 3
+
         if color == 2: #we are counting the white differential
-            return self.board.white_count - self.board.black_count
-        return self.board.black_count - self.board.white_count
+            return heuristic + self.board.white_count - self.board.black_count
+
+        return heuristic + self.board.black_count - self.board.white_count
 
 
 
@@ -62,6 +68,7 @@ class StudentAI():
                 self.board.make_move(move, opp_color) 
 
                 temp = self.get_heuristic(opp_color)
+                
                 if heuristic == None or temp > heuristic:
                     heuristic = temp
                     best_move = move
@@ -74,7 +81,6 @@ class StudentAI():
 
 
     def find_best_move(self, moves):
-
         heuristic = None
         best_move = None
         for piece in moves:
@@ -96,3 +102,46 @@ class StudentAI():
         
         self.board.make_move(best_move, self.color) 
         return best_move
+
+
+    def recur_best_move(self, moves, depth, color):
+        '''
+        print(f'depth = {depth} color = {color}')
+        print(moves)
+        print()
+        '''
+        if depth == 0:
+            return (None, self.get_heuristic(self.color))
+        elif len(moves) == 0:
+            return (None, float('-inf'))
+        heuristic = None
+        best_move = None
+        for piece in moves:
+            for move in piece:
+                #print(f'heuristic = {heuristic} depth = {depth}')
+                self.board.make_move(move, color)
+                #print(move)
+                curr_moves = self.board.get_all_possible_moves(self.opponent[color])
+                
+                
+                move1, temp = self.recur_best_move(curr_moves, depth-1, self.opponent[color])
+                if color == self.color:
+                    #Maximizing
+                    if heuristic is None or temp > heuristic:
+                        heuristic = temp
+                        best_move = move
+                else:
+                    #Minimizing
+                    if heuristic is None or temp < heuristic:
+                        heuristic = temp
+                        best_move = move
+
+                self.board.undo()
+                #print(f'ending heuristic = {heuristic} depth = {depth}')
+                
+
+        #print(f'returning {heuristic} depth = {depth}')
+        return (best_move, heuristic)
+
+
+
